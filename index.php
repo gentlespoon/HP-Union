6a68c5004a009f2a457f347968d116d775bebb9d
gentlespoon <songangda@gmail.com>
2017-07-08 10:58:18 -0700

    index updated

--- index.php
+++ index.php
@@ -4,12 +4,13 @@ include_once(ROOT."core/core.php");
 
 
 
-$r = DB("SELECT uid, username, regdate, qq FROM member ORDER BY uid ASC");
+$r = DB("SELECT uid, username, qq, email, regdate, lastlogin FROM member ORDER BY uid ASC");
 foreach ($r as $k => $v) {
   $r[$k]['regdate'] = toUserTime($r[$k]['regdate']);
+  $r[$k]['lastlogin'] = toUserTime($r[$k]['lastlogin']);
 }
-foreach ($r as $v) {
-  $body['text'] .= printv($v, true);
-}
+
+$body['text'] = "<br>".$lang['register'].$lang['user'].": ";
+$body['registered'] = $r;
 
 template("index");
