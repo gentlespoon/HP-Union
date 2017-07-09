c0314b9b114d203e2b2d12994c17c838732b984d
gentlespoon <songangda@gmail.com>
2017-07-09 05:28:29 -0700

    Much faster avatar load time

--- api/avatar2.php
+++ api/avatar2.php
@@ -1,6 +1,5 @@
 <?php
 error_reporting(E_ALL);
-header("Content-Type: image/jpeg");
 $avatar_url = "http://hp-union.com/static/images/default_avatar.jpg";
 
 $url = 'http://ptlogin2.qq.com/getface?appid=1006102&uin='.$_GET['qq'].'&imgtype=4';
@@ -9,5 +8,5 @@ $start_avatar = strpos($avatar_response, "http:");
 if ($start_avatar !== false) {
   $avatar_url = str_replace("\\", "", substr($avatar_response, $start_avatar, -4));
 }
-$avatar = file_get_contents($avatar_url);
-echo $avatar;
+// $avatar = file_get_contents($avatar_url);
+echo $avatar_url;
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -23,17 +23,20 @@ $(document).ready(function() {
     $("#modpwdform_password").val($.md5($("#modpwdform_password").val()));
   });
 
-  // // getUserAvatar( user_qq, dom_obj_id_to_place_avatar );
-  // function getUserAvatar(qq, obj) {
-  //   $.ajax({
-  //     url: "http://localhost/api/avatar.php?qq="+$("#user_qq").text(),
-  //   }).done(function(done) {
-  //     var avatar = done.replace(/\\/g, "");
-  //     $(obj).attr('src', avatar);
-  //     // alert(avatar);
-  //   });
-  // }
-  //
+  function getUserAvatar(qq, obj) {
+    // alert(qq+" "+obj);
+    $.ajax({
+      url: "http://hp-union.com/api/avatar2.php?qq="+qq,
+    }).done(function(done) {
+      var avatar = done.replace(/\\/g, "");
+      $(obj).attr('src', avatar);
+      // alert(avatar);
+    });
+  }
+
+  $(".avatar").each(function() {
+    getUserAvatar($(this).attr('qq'), "#"+$(this).attr('id'));
+  });
   // getUserAvatar($("#user_qq").text(), "#nav_avatar");
 
 });
