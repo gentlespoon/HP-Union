f5743ba3a97f67b6261c7da8b34c611f14359431
gentlespoon <songangda@gmail.com>
2017-07-25 10:39:00 -0700

    Updated nothing.

---
+++ admin.php
@@ -0,0 +1,22 @@
+<?php
+
+define("ROOT", $_SERVER['DOCUMENT_ROOT']."/");
+include(ROOT."core/core.php");
+
+
+switch ($_GET['act']) {
+
+
+  case "sitesettings":
+  default:
+
+
+
+
+}
+
+
+
+
+
+template("admin");
