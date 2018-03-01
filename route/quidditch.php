088939b4dcf613174543c5d24bdd55cb46be49f1
Angda Song <songangda@gmail.com>
2018-02-28 23:37:37 -0800

    魁地奇大概是这么拼？

---
+++ route/quidditch.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author : GentleSpoon <me.github@gentlespoon.com>
+ *
+ * File   : quidditch.php [HP-Union]
+ * Date   : 2018-02-28
+ * Time   : 23:37
+ */
+
+
+if (!defined('ROOT')) {
+  define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
+  require_once(ROOT . 'framework/http.php');
+  HTTP::error(403);
+}
+
+
+echo "Quidditch";
