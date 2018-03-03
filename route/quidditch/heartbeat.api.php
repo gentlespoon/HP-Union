2d9797c61428f1377723761082121d6723a03010
Angda Song <songangda@gmail.com>
2018-03-03 00:25:52 -0800

    。

--- route/quidditch/heartbeat.api.php
+++ route/quidditch/heartbeat.api.php
@@ -15,24 +15,24 @@ if (!defined('ROOT')) {
 }
 
 
-switch(rand(1,5)) {
+switch(rand(1,6)) {
   case 1:
     apiPrint(1, "如果消息变了的话说明你眼花了…");
-    exit();
+    break;
   case 2:
     apiPrint(1, "如果消息变了的话说明你眼花了…");
-    exit();
+    break;
   case 3:
     apiPrint(1, "也可能是你手机抽了…");
-    exit();
+    break;
   case 4:
     apiPrint(1, "如果你看到了这条消息…那说明你成功连接上服务器了…");
-    exit();
+    break;
   case 5:
     apiPrint(1, "消息会变哦╮(╯▽╰)╭");
-    exit();
+    break;
   default:
     apiPrint(1, "可能服务器出问题了…");
-    exit();
+    break;
 }
 
